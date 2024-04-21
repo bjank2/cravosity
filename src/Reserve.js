ea367ee0382e2531a926a14bcf8db99942251356
Rafay Khan <113397590+rafaymk02@users.noreply.github.com>
2024-04-21 00:05:56 -0500

    updated reserve.js

--- src/Reserve.js
+++ src/Reserve.js
@@ -71,7 +71,7 @@ function Reserve({ onNavigate }) {
           </select>
         </div>
         <button type="submit" className="orange-button">
-        <button onClick={() => onNavigate('GroupDining')} className="orange-button">Back</button>
+        <button onClick={() => onNavigate('Home')} className="orange-button">Back</button>
 
           Submit
         </button>
